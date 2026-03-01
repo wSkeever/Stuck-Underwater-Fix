292285e72bc25b173788cd0106ab42e81373a110
wSkeever <theregisteredone@gmail.com>
2026-02-28 21:35:02 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Stuck Underwater
+
+A smol SKSE plugin that fixes underwater visuals and sounds being stuck. Built from mrowrpurr's [Hello World](https://github.com/SkyrimScripting/SKSE_Template_HelloWorld) template and [CommonLibVR](https://github.com/alandtse/CommonLibVR).
---
+++ plugin.cpp
@@ -0,0 +1,53 @@
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
+using namespace RE;
+
+namespace StuckUnderwater {
+    struct detail {
+        static void UpdateUnderWaterVariables(TESWaterSystem* a_manager, bool a_underWater, float a_waterHeight) {
+            using func_t = decltype(&UpdateUnderWaterVariables);
+            static REL::Relocation<func_t> func{ RELOCATION_ID(31409, 32216) };
+            func(a_manager, a_underWater, a_waterHeight);
+        }
+    };
+
+    struct ProcessInWater {
+        static bool thunk(PlayerCharacter* a_actor, hkpCollidable* a_collidable, float a_waterHeight, float a_deltaTime) {
+            const auto camera = PlayerCamera::GetSingleton();
+            if (!camera) {
+                return func(a_actor, a_collidable, a_waterHeight, a_deltaTime);
+            }
+            const auto cameraBody = camera->GetRuntimeData().rigidBody;
+            if (!cameraBody) {
+                return func(a_actor, a_collidable, a_waterHeight, a_deltaTime);
+            }
+            float worldWaterHeight{ NI_INFINITY };
+            hkVector4 position;
+            cameraBody->GetPosition(position);
+            const float cameraHeight = position.quad.m128_f32[2];
+            const bool underWater = a_waterHeight > cameraHeight;
+            if (underWater) {
+                worldWaterHeight = a_waterHeight * bhkWorld::GetWorldScaleInverse();
+            }
+            detail::UpdateUnderWaterVariables(TESWaterSystem::GetSingleton(), underWater, worldWaterHeight);
+            return underWater;
+        }
+        static inline REL::Relocation<decltype(thunk)> func;
+        static constexpr std::size_t idx{ 0x9C };
+    };
+
+    void InstallUnderwaterFix() {
+        REL::Relocation<std::uintptr_t> vtbl{PlayerCharacter::VTABLE[0]};
+        ProcessInWater::func = vtbl.write_vfunc(ProcessInWater::idx, ProcessInWater::thunk);
+    }
+
+    SKSEPluginLoad(const SKSE::LoadInterface* skse) {
+        SKSE::Init(skse);
+        SKSE::GetMessagingInterface()->RegisterListener([](SKSE::MessagingInterface::Message* message) {
+            if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+                InstallUnderwaterFix();
+            }
+        });
+        return true;
+    }
+}
